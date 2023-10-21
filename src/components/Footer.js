fdd61364f66eccf2e657668abd7d3d4986940986
Diana Petrovčíková <63262388+petrovcikovadiana@users.noreply.github.com>
2023-10-21 21:26:11 +0200

    commit new-feature-img

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div>
+        <p class="bg-black p-7 text-xs text-white text-center">
+          © 2023 E-shop. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
